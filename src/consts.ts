a3b94e5ee4df9a8844592d4c2eeda795ce70b33a
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2023-03-16 11:35:03 +0100

    seo description

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = "Web Experiment - Raphael Ferrand digital garden";
 export const SITE_DESCRIPTION =
-  "A digital garden, a lab to experiment with all the new coll and fun features shipped every week to the web.";
+  "A digital garden, a lab 🥼 to experiment 🧪 with all the new cool and fun features shipped every week to the web 🎉";
 export const SITE_URL = "https://web-experiment.netlify.app";
 export const SITE_AUTHOR_NAME = "Raphaël Ferrand";
 export const SITE_AUTHOR_EMAIL = "goldenraphti@gmail.com";
